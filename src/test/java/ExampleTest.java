f152200579cb879e229a0159457fe83357a29c83
chen yan <yan925@gmail.com>
2013-04-27 14:54:48 +0800

    Added a example test
    try the framework, Junit.

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ExampleTest {
+    @Test
+    public void test_framework() throws Exception {
+        assertThat(1,is(1));
+    }
+}
