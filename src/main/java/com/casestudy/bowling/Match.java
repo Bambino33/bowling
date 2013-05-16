12e87ed7e8ab51682d6804389a1598d0cc31879b
ZengLu <jason0712@126.com>
2013-05-16 14:23:09 +0800

    Test the spare and strike round
    dev a new class match

---
+++ src/main/java/com/casestudy/bowling/Match.java
@@ -0,0 +1,70 @@
+package com.casestudy.bowling;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Zhengjie
+ * Date: 13-5-15
+ * Time: 下午3:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class Match {
+    public void hit(int bottles) {
+        if (FirstHitInRound) {
+            CurrentRound++;
+            BottlesDown[2*(CurrentRound-1)] = bottles;
+            FirstHitInRound = false;
+            if(10 == bottles){
+                FirstHitInRound = true;
+            }
+        }
+        else {
+            BottlesDown[2*(CurrentRound-1)+1] = bottles;
+            FirstHitInRound = true;
+        }
+        BottlesDownInOneRound = 0;
+        BottlesDownInOneRound = BottlesDown[2*(CurrentRound-1)] + BottlesDown[2*(CurrentRound-1)+1];
+
+        if (BottlesDownInOneRound > 10){
+            if (FirstHitInRound){
+                BottlesDown[2*(CurrentRound-1)+1] = 0;
+            }
+            else {
+                BottlesDown[2*(CurrentRound-1)] = 0;
+            }
+            throw new UnsupportedOperationException("BottlesDownInOneRound is larger than 10");
+        }
+    }
+
+    public int getScore() {
+        score = 0;
+        for (int i = 1; i <= CurrentRound; i++)     {
+            BottlesDownInOneRound = 0;
+            BottlesDownInOneRound =  BottlesDown[2*(i-1)] + BottlesDown[2*(i-1)+1];
+            if(BottlesDownInOneRound < 10 ){
+                score += BottlesDownInOneRound;
+            }
+            else
+            {
+                if(BottlesDownInOneRound == 10){
+                    if(10 == BottlesDown[2*(i-1)] ){
+                        int Next2HitBottles = BottlesDown[2*i]+ BottlesDown[2*i+1];
+                        score += BottlesDownInOneRound +Next2HitBottles;
+                    }
+                    else{
+                        int NextHitBottles = BottlesDown[2*i];
+                        score += BottlesDownInOneRound +NextHitBottles;
+                    }
+                }
+            }
+        }
+        return score;
+    }
+
+    private int score = 0;
+    private final int MaxHitNum = 21;
+    private int[] BottlesDown = new int[MaxHitNum];
+    private int BottlesDownInOneRound = 0;
+    private int CurrentRound = 0;
+    private boolean FirstHitInRound = true;
+
+}
---
+++ src/test/java/com/casestudy/bowling/MatchTest.java
@@ -0,0 +1,76 @@
+package com.casestudy.bowling;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Zhengjie
+ * Date: 13-5-15
+ * Time: 下午3:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class MatchTest {
+    @Test
+    public void should_sum_scores_for_normal_round() throws Exception {
+        Match match = new Match();
+        match.hit(4);
+        assertThat(match.getScore(), is(4));
+        match.hit(3);
+        assertThat(match.getScore(), is(7));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void should_not_sum_score_larger_than_ten() throws Exception {
+        Match match = new Match();
+        match.hit(11);
+    }
+
+    @Test
+    public void should_not_sum_score_if_found_total_score_is_larger_than_10() throws Exception {
+        Match match = new Match();
+        match.hit(6);
+        try {
+            match.hit(5);
+        } catch (Exception e) {
+
+        }
+        assertThat(match.getScore(), is(6));
+    }
+
+    @Test
+    public void should_sum_scores_for_two_normal_round() throws Exception {
+        Match match = new Match();
+        match.hit(4);
+        assertThat(match.getScore(), is(4));
+        match.hit(3);
+        assertThat(match.getScore(), is(7));
+        match.hit(4);
+        assertThat(match.getScore(), is(11));
+        match.hit(3);
+        assertThat(match.getScore(), is(14));
+    }
+
+    @Test
+    public void should_sum_scores_for_spare_round() throws Exception {
+        Match match = new Match();
+        match.hit(4);
+        assertThat(match.getScore(), is(4));
+        match.hit(6);
+        match.hit(4);
+        match.hit(3);
+        assertThat(match.getScore(), is(21));
+    }
+
+    @Test
+    public void should_sum_scores_for_strike_round() throws Exception {
+        Match match = new Match();
+        match.hit(10);
+        assertThat(match.getScore(), is(10));
+        match.hit(4);
+        match.hit(3);
+        assertThat(match.getScore(), is(24));
+    }
+}
