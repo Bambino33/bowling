64c63bcac468bd54f5839ea7ea0907225cc1cda9
chen yan <yan925@gmail.com>
2013-05-17 14:05:50 +0800

    formatted Round

--- src/main/java/com/casestudy/bowling/Round.java
+++ src/main/java/com/casestudy/bowling/Round.java
@@ -1,15 +1,16 @@
 package com.casestudy.bowling;
 
-public class Round  {
+public class Round {
     public void hit(int score) {
-           if(this.score+score>10){
-               throw new UnsupportedOperationException("Your score is larger than 10");
-           }
-           this.score += score;
+        if (this.score + score > 10) {
+            throw new UnsupportedOperationException("Your score is larger than 10");
+        }
+        this.score += score;
     }
 
     public int getScore() {
         return score;
     }
-        private int score = 0;
+
+    private int score = 0;
 }
