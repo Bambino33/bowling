1976f112aafe0364bb6e535657cca18aaefbe528
chen yan <yan925@gmail.com>
2013-05-17 14:04:14 +0800

    refactoried RoundTest
    Added setUp method, this method will be called before each test running.

--- src/test/java/com/casestudy/bowling/RoundTest.java
+++ src/test/java/com/casestudy/bowling/RoundTest.java
@@ -1,14 +1,22 @@
 package com.casestudy.bowling;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class RoundTest {
+
+    private Round round;
+
+    @Before
+    public void setUp() throws Exception {
+        round = new Round();
+    }
+
     @Test
     public void should_sum_scores_for_normal_round() throws Exception {
-        Round round = new Round();
         round.hit(4);
         assertThat(round.getScore(), is(4));
         round.hit(3);
@@ -24,13 +32,12 @@ public class RoundTest {
 
     @Test
     public void should_not_sum_score_if_found_total_score_is_larger_than_10() throws Exception {
-        Round round  = new Round();
         round.hit(6);
-        try{
+        try {
             round.hit(5);
-        }   catch(Exception e){
+        } catch (Exception e) {
 
         }
-        assertThat(round.getScore(),is(6));
+        assertThat(round.getScore(), is(6));
     }
 }
